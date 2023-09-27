702afbaff67ec8300fbabab910c686baba52e1f3
ElezthemDev <97851451+Elezthem@users.noreply.github.com>
2023-09-27 07:18:25 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "RU");
+	
+	int *num = new int[80];
+	num[0] = 22;
+	cout << num[0] << endl;
+	delete[] num;
+	cout << "Element: " << num[0] << endl;
+
+	return 0;
+}
